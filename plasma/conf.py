7ae7baf071ef7e4e400ee8549398580319410464
Wouter Van de Pontseele <wvdp@tigergpu.princeton.edu>
2019-08-09 06:38:25 -0400

    Added yaml path to work with local setup

--- plasma/conf.py
+++ plasma/conf.py
@@ -2,19 +2,12 @@ from plasma.conf_parser import parameters
 import os
 import errno
 
-print('\nlooking for conf file:')
-print(__file__)
-print(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'examples/conf.yaml'))
-print(os.path.exists(os.path.join(os.path.abspath(
-    os.path.dirname(__file__)), 'examples/conf.yaml')))
-
 conf_file = ''
 if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../examples/conf.yaml')):
     conf_file = os.path.join(os.path.abspath(
         os.path.dirname(__file__)), '../examples/conf.yaml')
-elif os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'examples/conf.yaml')):
-    conf_file = os.path.join(os.path.abspath(
-        os.path.dirname(__file__)), 'examples/conf.yaml')
+elif os.path.exists('../plasma-python/examples/conf.yaml'):
+    conf_file = '../plasma-python/examples/conf.yaml'
 elif os.path.exists('./conf.yaml'):
     conf_file = './conf.yaml'
 elif os.path.exists('./examples/conf.yaml'):
